3862f4c1b46118c729bbb9c35cedf982d7fe7d72
luis_nigrinis <nigrinisluis@gmail.com>
2021-11-07 01:26:37 -0500

    omse agrego librerias sensor_ultrasonico_1.c y .h

---
+++ PROYECYO_FINAL_IoT__K32L2B31A_Project/iot_sdk/iot_sdk_peripherals/sensor_ultrasonico_dp1.h
@@ -0,0 +1,46 @@
+/*! @file : sensor_ultrasonico_dp1.h
+ * @author  Luis Carlos Nigrinis Alvarez
+ * @version 1.0.0
+ * @date    6/11/2021
+ * @brief   Driver para 
+ * @details
+ *
+ */
+#ifndef IOT_SDK_PERIPHERALS_SENSOR_ULTRASONICO_DP1_H_
+#define IOT_SDK_PERIPHERALS_SENSOR_ULTRASONICO_DP1_H_
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "peripherals.h"
+
+/*!
+ * @addtogroup X
+ * @{
+ */
+/*!
+ * @addtogroup X
+ * @{
+ */
+/*******************************************************************************
+ * Public Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Public vars
+ ******************************************************************************/
+ //float sensorultrasonicobtenerdatodp1(void);
+ void Sensorultrasonico_1_init(void);
+ void Sensorultrasonico_1_Task_Run(void);
+/*******************************************************************************
+ * Public Prototypes
+ ******************************************************************************/
+
+
+/** @} */ // end of X group
+/** @} */ // end of X group
+
+#endif /* IOT_SDK_PERIPHERALS_SENSOR_ULTRASONICO_DP1_H_ */
