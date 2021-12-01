4827eb44a917610262547a5e67659f320455c6c7
Jose Miguel Morales <josemiguelmv99@gmail.com>
2021-12-01 13:05:41 -0500

    modificacion libreria control_temperatura.c,modificacion de la  funcion de captura de datos de temperatura cada 300 ms

--- PROYECYO_FINAL_IoT__K32L2B31A_Project/iot_sdk/iot_sdk_temperatura/control_temperatura.c
+++ PROYECYO_FINAL_IoT__K32L2B31A_Project/iot_sdk/iot_sdk_temperatura/control_temperatura.c
@@ -15,10 +15,20 @@
 #include "board.h"
 #include "control_temperatura.h"
 #include "leds.h"
+#include "fsl_spi.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "fsl_debug_console.h"
 
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+#define EXAMPLE_SPI_MASTER              SPI0
+#define EXAMPLE_SPI_MASTER_SOURCE_CLOCK kCLOCK_BusClk
+#define EXAMPLE_SPI_MASTER_CLK_FREQ     CLOCK_GetFreq(kCLOCK_BusClk)
+
+#define BUFFER_SIZE (2)
 
 
 /*******************************************************************************
@@ -34,103 +44,88 @@
 /*******************************************************************************
  * Local vars
  ******************************************************************************/
+static uint8_t srcBuff[BUFFER_SIZE];
+static uint16_t destBuff[BUFFER_SIZE];
+
 volatile float sensor_temperatura;
+uint16_t dato_original;
+uint16_t dato_entero;
+uint16_t dato_final_entero;
+uint16_t dato_final_decimal;
+uint16_t dato_decimal ;
+float temperatura_grados ;
+
+spi_master_config_t userConfig = {0};
+uint32_t srcFreq               = 0;
+//uint32_t i                     = 0;
+//uint32_t err                   = 0;
+spi_transfer_t xfer            = {0};
 
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
+void Sensor_temperatura_Init(void){
+
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitDebugConsole();
+    PRINTF("\n\rMaster Start...\n\r");
+    /*
+     * userConfig.enableStopInWaitMode = false;
+     * userConfig.polarity = kSPI_ClockPolarityActiveHigh;
+     * userConfig.phase = kSPI_ClockPhaseFirstEdge;
+     * userConfig.direction = kSPI_MsbFirst;
+     * userConfig.dataMode = kSPI_8BitMode;
+     * userConfig.txWatermark = kSPI_TxFifoOneHalfEmpty;
+     * userConfig.rxWatermark = kSPI_RxFifoOneHalfFull;
+     * userConfig.pinMode = kSPI_PinModeNormal;
+     * userConfig.outputMode = kSPI_SlaveSelectAutomaticOutput;
+     * userConfig.baudRate_Bps = 500000U;
+     */
+    SPI_MasterGetDefaultConfig(&userConfig);
+    userConfig.dataMode = kSPI_16BitMode;
+    userConfig.outputMode = kSPI_SlaveSelectAutomaticOutput;
+    userConfig.direction = kSPI_MsbFirst;
+    userConfig.pinMode = kSPI_PinModeNormal;
+
+    srcFreq = EXAMPLE_SPI_MASTER_CLK_FREQ;
+    SPI_MasterInit(EXAMPLE_SPI_MASTER, &userConfig, srcFreq);
+}
+
+
+float Sensor_temperatura_Captura_de_datos(void){
+	if(tiempo_capturadato_temperatura==100){
+		 /*Start Transfer*/
+		xfer.txData   = srcBuff;
+		xfer.rxData   = destBuff;
+		xfer.dataSize = BUFFER_SIZE;
+		SPI_MasterTransferBlocking(EXAMPLE_SPI_MASTER, &xfer);
+		encender_led_rojo();
+	}
+	if(tiempo_capturadato_temperatura > 300){
+		apagar_led_rojo();
+		tiempo_capturadato_temperatura = 0;
+		dato_original = destBuff[0] ;
+		dato_decimal = dato_original & 0x32;
+		dato_final_decimal  = dato_decimal>>3;
+		dato_entero = dato_original & 0b0000111111100000;
+		dato_final_entero = dato_entero>>5;
+		temperatura_grados = dato_final_entero + dato_final_decimal*0.1;
+		//printf("16_bits---%d\r\n",dato_original);
+		//printf("%0.2f\r\n",temperatura_grados);
+	}
+
+	return(temperatura_grados);
+}
 
 
 /*******************************************************************************
  * Public Source Code
  ******************************************************************************/
-int j = 0 ;
-int i = 0 ;
-
-void Sensor_temperatura_init(void)
-{
-   GPIO_PinWrite(GPIOD,7,0); // SCK
-   GPIO_PinWrite(GPIOE,0,1); // CS
-}
-
-void delay_ms_block(){
-	uint32_t i;
-	for(i=0;i<0x1063;i++);
-}
-
-void delay_us_block(){
-	uint32_t j;
-	for(j=0;j<0x5;j++);
-}
-
-void delay_200ms_block(){
-	uint32_t l;
-	for(l=0;l<0xCCCCD;l++);
-}
-
- 
-
-
-uint16_t read_sensor_temperatura(void){
-	int k = 0 ;
-	uint16_t buffer[16];
-	float temperatura_decimal ;
-	uint16_t temperatura_entero ;
-	float temperatura_max6675 ;
-	uint16_t dato_12, dato_11, dato_10, dato_9, dato_8, dato_7, dato_6, dato_5, dato_4, dato_3, dato_decima_2, dato_decima_1;
-	//uint16_t datos_temperatura ;
-	GPIO_PinWrite(GPIOE,0,0); // CS
-	delay_ms_block();
-	for (k=0;k<16;k++){
-		//shift_left(&data,2,GPIO_PinRead(GPIOE,1));      //reads in 2 bytes to data from the pin TC_DATA
-		buffer[k] = GPIO_PinRead(GPIOE,1);
-		//datos_temperatura = buffer[k];
-		GPIO_PinWrite(GPIOD,7,1); // SCK;
-		delay_us_block();
-		//delay_us(1); //Descomentar si usa crystal mayor a 10MHz
-		GPIO_PinWrite(GPIOD,7,0); // SCK;
-		delay_us_block();
-		//delay_us(1);
-		apagar_led_verde();
-		//printf("The %d is wrong! data is %d\n\r", k, buffer[k]);
-		if(k==15){
-			encender_led_verde();
-		}
-	}
-	delay_ms_block();
-	//printf("%d%d%d%d%d%d%d%d%d%d%d%d\n\r",buffer[1],buffer[2],buffer[3],buffer[4],buffer[5],buffer[6],buffer[7],buffer[8],buffer[9],buffer[10],buffer[11],buffer[12]);
-	GPIO_PinWrite(GPIOE,0,1); // CS
-	delay_ms_block();
-	dato_12 = 512 * buffer[1] ;
-	dato_11 = 256 * buffer[2] ;
-	dato_10 = 128 * buffer[3] ;
-	dato_9 = 64 * buffer[4] ;
-	dato_8 = 32 * buffer[5] ;
-	dato_7 = 16 * buffer[6] ;
-	dato_6 = 8 * buffer[7] ;
-	dato_5 = 4 * buffer[8] ;
-	dato_4 = 2 * buffer[9] ;
-	dato_3 = 1 * buffer[10] ;
-	dato_decima_2 = 2 * buffer[11] ;
-	dato_decima_1 = 1 * buffer[12] ;
-	temperatura_entero = dato_12 + dato_11 + dato_10 + dato_9 + dato_8 + dato_7 + dato_6 + dato_5 + dato_4 + dato_3;
-	temperatura_decimal = (dato_decima_2 + dato_decima_1)*0.1 ;
-	temperatura_max6675 = temperatura_entero + temperatura_decimal ;
-	//printf("%d , %0.2f , %0.2f\n\r",temperatura_entero, temperatura_decimal, temperatura_max6675);
-  return(temperatura_max6675);
-}
-
-
-
-
-float do_everything(void){
-	Sensor_temperatura_init();
-    delay_200ms_block(); //200ms is a long time to be doing nothing. use a timer interrupt to avoid wasting time here
-   return(read_sensor_temperatura());
-}
 
 
 void Sensor_temperatura_Task_Run(void){
-	  sensor_temperatura = do_everything();
+	  sensor_temperatura = Sensor_temperatura_Captura_de_datos();
 	  //printf("%0.2f\r\n",sensor_temperatura);
 }
